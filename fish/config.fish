5d6b4f7e561a836346072748069e113b2501954d
n-ari <fukunari.r.aa@m.titech.ac.jp>
2020-09-19 23:31:30 +0900

    add fish config

---
+++ fish/config.fish
@@ -0,0 +1,5 @@
+function fish_user_key_bindings
+end
+
+# opam configuration
+source $HOME/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
--- install.sh
+++ install.sh
@@ -5,10 +5,15 @@ for f in .??*; do
   [ "$f" = ".git" ] && continue
   [ "$f" = ".gitignore" ] && continue
   [ "$f" = ".config" ] && continue
+  [ "$f" = "fish" ] && continue
   ln -snfv "$HOME"/dotfiles/"$f" "$HOME"/"$f"
 done
 
 for f in `find .config -maxdepth 1 -mindepth 1`; do
   ln -snfv "$HOME"/dotfiles/"$f" "$HOME"/"$f"
+done
+
+for f in `find fish -maxdepth 1 -mindepth 1`; do
+  ln -snfv "$HOME"/dotfiles/"$f" "$HOME"/.config/"$f"
 done
 
