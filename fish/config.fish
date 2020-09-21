c11266a7381f4a61491befa5d60431e265607501
n-ari <fukunari.r.aa@m.titech.ac.jp>
2020-09-21 18:00:23 +0900

    add bobthefish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,10 @@
 function fish_user_key_bindings
 end
 
+# bobthefish setting
+set -g theme_powerline_fonts no
+set -g theme_nerd_fonts yes
+set -g theme_display_ruby no
+
 # opam configuration
 source $HOME/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
